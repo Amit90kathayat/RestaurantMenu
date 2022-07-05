eeabac3b2164cd0f70b95322b6516dcb62c357ba
Amit <amitkathayat90@gmail.com>
2022-07-05 16:12:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturant from './component/basics/Resturant';
+
+const App = () => {
+  return (
+    <>
+    <Resturant />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/Breadomlet.jpg",
+      name: "Breadomlet",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Our breakfast bread omelette recipe is a versatile dish that you can customize to fit your needs. The major health benefits of omelettes are that they are packed with protein and healthy fats. Eggs help in regulating cholesterol levels in the body and improve eyesight since they are high in Vitamin A. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/aloopakoda.jpg",
+      name: "aloo pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo pakoda are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji. ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Corn is the second most plentiful cereal grown for human consumption, and many cultures around the world have lived on this grain. Corn is a versatile crop, and everything on a corn plant is useable. No part of the corn is wasted. The husk of the corn is traditionally used in making tamales ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chole",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "All us fans of chole bhature know that it isn't the healthiest thing in the world. The bhaturas are made using refined flour or maida and deep-fried in desi ghee or oil. On the other hand, the chole is prepared using so many spices and oil. One plate of chole bhature has 500 calories. ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients. ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A non-vegetarian diet includes chicken, meat, eggs and fish. A non-vegetarian diet also has several health benefits because this type of food is rich in protein and vitamin B. . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Indian sweets are collectively called Mithai which is derived from the word mitha which means sweet Ranging from Gulab Jamuns to Payasam, nostalgic favourites to modern avatars, we bring to you the most loved 15 Indian desserts.. ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "They are a rich source of protein, carbohydrates and fibres. Furthermore, they are rich in minerals and vitamins such as copper, folate, potassium, iron and vitamin K1. Due to the presence of protein and fibre, rajma can support healthy weight management by improving digestion and providing energy. . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Samosas are great because of the combination of the crispy outside, which forms during frying, and the flavourful filling on the inside. There are a lot of different samosa fillings out there and really, you can make whatever you like . ",
+    },
+  ];
+
+  export default Menu;
